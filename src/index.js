c706a4dd0efc0ffe289c4d7ea33cd4349da454e6
Jeff Mendez <jeffmendez19@gmail.com>
2020-04-21 00:04:55 -0400

    inline current domain scripts

--- src/index.js
+++ src/index.js
@@ -9,16 +9,32 @@ const { stdTTL, headers } = require("./config");
 const cache = new NodeCache({ stdTTL });
 
 // Experimental manipulation
-// function manipulateSource(i, src, url) {
-//   if (src) {
-//     if (!src.includes(url)) {
-//       const newSrc = src.replace(/^[/]+/, "");
-//       src = `${newSrc.includes("http") ? "" : url + "/"}` + newSrc;
-//     }
-//     return src;
-//   }
-//   return null;
-// }
+function manipulateSource(i, src, url, $html) {
+  if (src) {
+    const isSlash = src[0] === "/";
+
+    async function grabData() {
+      if (isSlash) {
+        const pathUrl = `${url}${isSlash ? "" : "/"}${src}`;
+
+        const scriptCode = await fetch(pathUrl, {
+          uri: pathUrl,
+          headers,
+        });
+
+        const scriptText = await scriptCode.text();
+
+        $html(`script[src="${src}"]`).html(scriptText);
+        return true;
+      }
+    }
+
+    grabData();
+
+    return src;
+  }
+  return null;
+}
 
 async function renderHtml({ url, baseHref }) {
   const cachedHtml = await cache.get(url);
@@ -35,15 +51,16 @@ async function renderHtml({ url, baseHref }) {
       });
       const html = await response.text();
       const $html = cheerio.load(html);
-      // response.headers.has('access-control-allow-origin')
 
       if ($html) {
         $html("head").prepend(`<base target="_self" href="${url}">`);
+
         if (typeof baseHref !== "undefined" && baseHref !== "false") {
-          $html("script").attr("crossorigin", "anonymous");
-          // $html('script').attr('src', (i, src) =>
-          //   manipulateSource(i, src, url)
-          // )
+          // $html('script').attr('crossorigin', 'anonymous')
+          $html("script").attr("src", (i, src) =>
+            manipulateSource(i, src, url, $html)
+          );
+
           // $html('link').attr('href', (i, src) =>
           //   manipulateSource(i, src, url)
           // )
